43360b634d3d73215d4a5033221e299de4fcae5c
Maciej Ludwiczak <maciej.ludwiczak92@gmail.com>
2025-01-15 17:36:05 +0100

    work-connect p8

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/work-connect",
+  base: "/work-connect/",
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
